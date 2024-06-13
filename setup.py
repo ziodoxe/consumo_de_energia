667394b2a5851c6a43495d6c9df45be4d412b4ba
ziodoxe <koala0000@hotmail.com>
2024-06-13 20:22:08 -0300

    Update setup.py
    Armado de setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,33 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='consumo de energia',
     packages=find_packages(),
     version='0.1.0',
-    description='{{ cookiecutter.description }}',
-    author='{{ cookiecutter.author_name }}',
+    description='Análisis y predicción del consumo de energía',
+    author='Marcelo Adrian Cussi',
     license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
+    install_requires=[
+        # Lista de dependencias del proyecto
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'matplotlib',
+        'seaborn'
+    ],
+    classifiers=[
+        # Clasificadores que ayudan a otros usuarios a encontrar tu paquete
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.11.1',
 )
