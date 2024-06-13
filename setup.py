f87d024504ece6b4e7556d44d87e25578e9b4cd9
ziodoxe <koala0000@hotmail.com>
2024-06-13 20:26:29 -0300

    Update setup.py
    Licencia MIT

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1.0',
     description='Análisis y predicción del consumo de energía',
     author='Marcelo Adrian Cussi',
-    license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
+    license='MIT',
     install_requires=[
         # Lista de dependencias del proyecto
         'numpy',
